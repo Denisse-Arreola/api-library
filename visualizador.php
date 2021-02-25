5bbf29707630bd36554b7c176d2ec274f22bbd76
danielgrcXO <danielitoelguarneritos@gmail.com>
2021-02-25 12:04:25 -0800

    visualizador de pdf

---
+++ visualizador.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html Lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Visualizador</title>
+    <link rel="stylesheet" href="visualizador.css">
+</head>
+<body>
+
+ <section>
+  <h1><center>Visualizar el pdf hahaha</center></h1>
+
+    <center>  <iframe src="Puntos.pdf"></iframe>   </center>
+ </section>
+  
+
+</body>
+
+
+
+
+
+</html>
