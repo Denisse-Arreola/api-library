3220b94220b77d455b186c75aabc4c178fbdc738
Denisse-Arreola <arxie.denisse@gmail.com>
2021-02-27 13:55:58 -0800

    clase de login

--- php/classes/testing.php
+++ php/classes/testing.php
@@ -5,9 +5,9 @@ require_once("user.php");
 echo "Esto es una prueba";
 echo "</br>";
 
-var_dump($testing = new usuario("arxie.denisse@gmail.com","12345"));
+$testing = new usuario("arxie.denisse@gmail.com","12345");
 
-
+echo json_encode($testing);
 
 
 
