91c80c87b4ad3d1cf73f32dbf993d8fbc0e6c8ba
Tonatiu010 <l.tonatiuh010@gmail.com>
2021-03-05 14:41:51 -0800

    "Modulo Prueba bookSpace"

---
+++ php/bookSpace.php
@@ -0,0 +1,54 @@
+<?php
+
+// Pruebas con una sección de Api en PHP
+// buscamos un parámetro que establezca una 
+// acción a seguir.
+
+function retError(){
+    echo json_encode(Array(
+            'error' => 'No params settled.',
+            'res' => false
+        )
+    );    
+
+}
+
+
+if (isset($_GET['book_action'])) { // Leemos la Acción
+
+    if ( $_GET['book_action'] == 'set_book' && isset( $_GET['book_ID'] ) ){
+        // Caso: Establecer un libro global En las Cookies.
+
+        setcookie('book_ID', $_GET['book_ID']);
+        
+        echo json_encode(Array(
+                'status' => 'Libro en Índice.',
+                'res' => true
+            )
+        );
+    
+
+    } else if ($_GET['book_action'] == 'get_book'){
+        // Caso: Extraer un libro global En las Cookies.
+        
+        
+        echo json_encode(Array(
+                'id' => $_COOKIE['book_ID'],
+                'status' => true
+            )
+        );
+
+    } else {
+        retError();
+    }
+
+
+} else {
+
+    retError();
+
+}
+
+
+
+?>
