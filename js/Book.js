38a6c32ebe4862afdaebd76975b338b89f140660
Denisse-Arreola <arxie.denisse@gmail.com>
2021-03-08 15:27:20 -0800

    show book html

--- js/Book.js
+++ js/Book.js
@@ -22,13 +22,13 @@ class Book {
             book.addEventListener("click", 
                 event => {
                     //console.log('http://localhost:8080/api-library/php/api.php?book_action=get_book&book_ID='+json.ID)
-                    fetch('http://localhost/api-library/php/api.php?book_action=set_book&book_ID='+json.ID).
-                    //fetch('http://localhost:8080/api-library/php/api.php?book_action=set_book&book_ID='+json.ID).
+                    //fetch('http://localhost/api-library/php/api.php?book_action=set_book&book_ID='+json.ID).
+                    fetch('http://localhost:8080/api-library/php/api.php?book_action=set_book&book_ID='+json.ID).
                     then(res => res.json()). 
                     then( 
                         data => {
-                            location.href = 'http://localhost/api-library/viewBook.html'
-                            //location.href = 'http://localhost:8080/api-library/viewBook.html'
+                            //location.href = 'http://localhost/api-library/viewBook.html'
+                            location.href = 'http://localhost:8080/api-library/viewBook.html'
                         }
                     )
 
